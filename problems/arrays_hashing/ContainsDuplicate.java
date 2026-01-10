d26969196387e6c96c8dca054c635483dff0d584
kkirangit <kkiran1633@gmail.com>
2026-01-10 15:55:01 +0530

    added examples

--- problems/arrays_hashing/ContainsDuplicate.java
+++ problems/arrays_hashing/ContainsDuplicate.java
@@ -1,8 +1,17 @@
 // neetcode 150
 // package problems.arrays_hashing;
 // ContainsDuplicate description
-// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.  
+// example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
 
+// example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+// example 3:
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
 import java.util.HashSet;
 
 public class ContainsDuplicate {
